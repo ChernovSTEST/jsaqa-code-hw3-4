c0afdc03046320c8add3d0a01c8c90380306d966
Test <test@test.com>
2023-10-18 10:39:11 +0300

    First

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,7 +1,7 @@
 const sorting = require("../../app");
 
-describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
+describe("Books names test suite", () => {
+  it("should sort books names in ascending order when nameA < nameB", () => {
     expect(
       sorting.sortByName([
         "Гарри Поттер",
@@ -12,6 +12,80 @@ describe("Books names test suit", () => {
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
+    ]);
+  });
+
+  it("should sort books names in ascending order when nameA > nameB", () => {
+    expect(
+      sorting.sortByName([
+        "B",
+        "C",
+        "A",
+      ])
+    ).toEqual([
+      "A",
+      "B",
+      "C",
+    ]);
+  });
+
+  it("should handle empty input array", () => {
+    expect(sorting.sortByName([])).toEqual([]);
+  });
+
+  it("should handle names with same case-insensitive values", () => {
+    expect(
+      sorting.sortByName([
+        "apple",
+        "Banana",
+        "Cherry",
+      ])
+    ).toEqual([
+      "apple",
+      "Banana",
+      "Cherry",
+    ]);
+  });
+
+  it("should handle names with special characters", () => {
+    expect(
+      sorting.sortByName([
+        "oranges",
+        "Apples",
+        "bananas!",
+      ])
+    ).toEqual([
+      "Apples",
+      "bananas!",
+      "oranges",
+    ]);
+  });
+
+  it("should handle names with Unicode characters", () => {
+    expect(
+      sorting.sortByName([
+        "Подарок",
+        "Волшебство",
+        "Яблоко",
+      ])
+    ).toEqual([
+      "Волшебство",
+      "Подарок",
+      "Яблоко",
+    ]);
+  });
+
+  it("should handle equal names", () => {
+    expect(
+      sorting.sortByName([
+        "A",
+        "A",
+        "A",
+      ])
+    ).toEqual([
+      "A",
+      "A",
+      "A",
     ]);
   });
 });
