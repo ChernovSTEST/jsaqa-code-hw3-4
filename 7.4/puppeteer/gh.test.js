3ab26650993f3885b5d1489d23e192acccc1e4f0
Test <test@test.com>
2023-11-02 19:46:05 +0300

    First

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -9,26 +9,30 @@ afterEach(() => {
   page.close();
 });
 
-describe("Github page tests", () => {
-  test("The h1 header content'", async () => {
-    const firstLink = await page.$("header div div a");
-    await firstLink.click();
-    await page.waitForSelector('h1');
-    const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+describe("Three new tests have been added", () => {
+  afterEach(() => {
+    page.close();
   });
 
-  test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
-    expect(actual).toEqual("#start-of-content");
-  });
+  test("The page About ", async () => {
+    await page.goto("https://github.com/about");
+    const title = await page.title();
+    expect(title).toContain("About · GitHub");
+  }, 30000);
 
-  test("The page contains Sign in button", async () => {
-    const btnSelector = ".btn-large-mktg.btn-mktg";
+  test("The page Pricing", async () => {
+    await page.goto("https://github.com/pricing");
+    const title = await page.title();
+    expect(title).toContain("Pricing · Plans for every developer · GitHub");
+  }, 30000);
+
+  test("The page contains Introduction to GitHub", async () => {
+    await page.goto("https://skills.github.com");
+    const btnSelector = (ss = ".btn.btn-primary.btn-large");
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
-  });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Introduction to GitHub");
+  }, 30000);
 });
